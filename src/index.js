3c74200d1006cc340327671ea2b966cb087e6338
bashar4980 <sagor.ahmed3669@gmail.com>
2022-11-24 20:30:16 +0600

    Environment is setup is done

--- src/index.js
+++ src/index.js
@@ -15,3 +15,12 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// echo "# b612-used-products-resale-clients-side-bashar4980" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/programming-hero-web-course-4/b612-used-products-resale-clients-side-bashar4980.git
+// git push -u origin main
